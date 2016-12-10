c194944b50f9ebb09651f80d9e050a0f44ca6220
藤本健悟 <kengoooon@Macbook-Air.local>
2016-12-10 13:34:32 +0900

    ・コメントの追記 ・不要な変数の削除

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -17,23 +17,21 @@ class ViewController: UIViewController {
     @IBOutlet weak var mulBtn: UIButton!
     @IBOutlet weak var divBtn: UIButton!
     
-    
+    //合計を保持する変数
     var sum:Float64 = 0
+    
+    //演算子を保持する変数
+    var ope:String = ""
+    
+    //入力結果を保持する変数
+    var result:String = ""
+    
+    //各種フラグ
     var dotFlg:Int = 0
     var opeFlg:Int = 0
-    var ope:String = ""
-    var result:String = ""
-    var ans:String = ""
-    var i = 0
-    var tmp:Float64 = 0
     var firstFlg = 0
     
-    //整数の時に小数点以下を表示しない関数
-//    func disdot(input:Float64) -> String{
-//        ans = String(format:"%g",input)
-//        return ans
-//    }
-    
+    //演算子フラグと小数点フラグをクリアする関数
     func flgClear() -> (){
         opeFlg = 0
         dotFlg = 0
@@ -49,21 +47,18 @@ class ViewController: UIViewController {
     
     
     //計算関数
-    func culc(input:String) -> (){
+    func calc(input:String) -> (){
         switch ope {
         case "+":
             sum += Float64(input)!
-            print(sum)
             result = String(format:"%g",sum)
             resultlbl.text = result
         case "-":
             sum -= Float64(input)!
-            print(sum)
             result = String(format:"%g",sum)
             resultlbl.text = result
         case "×":
             sum *= Float64(input)!
-            print(sum)
             result = String(format:"%g",sum)
             resultlbl.text = result
         case "÷":
@@ -83,11 +78,12 @@ class ViewController: UIViewController {
     
     //入力値をresultlblに表示
     @IBAction func zeroBtn(_ sender: Any) {
+        //演算子がなければ 文字列を前の入力と結合して表示
         if opeFlg == 0{
             result += "0"
             resultlbl.text = result
         }else{
-            //tmp　に　演算子があれば　新しく文字列を表示　tmpをクリア
+            //直前に演算子の入力があれば　新しく文字列を表示
             result = "0"
             resultlbl.text = result
             flgClear()
@@ -96,13 +92,10 @@ class ViewController: UIViewController {
 
     }
     @IBAction func oneBtn(_ sender: Any) {
-        //演算子がなければ 文字列を結合して表示していく
         if opeFlg == 0{
             result += "1"
             resultlbl.text = result
-            //
                 }else{
-            //演算子があれば　新しく文字列を表示　演算子フラグを0に
             result = "1"
             resultlbl.text = result
             flgClear()
@@ -232,7 +225,6 @@ class ViewController: UIViewController {
         firstFlg = 0
         ope = ""
         opeFlg = 0
-        tmp = 0
         bouderClear()
         
     }
@@ -242,7 +234,7 @@ class ViewController: UIViewController {
     //足し算
     @IBAction func additionBtn(_ sender: Any) {
         if opeFlg == 0{
-            culc(input: result)
+            calc(input: result)
             ope = "+"
             opeFlg = 1
             addBtn.layer.borderWidth = 2.0;
@@ -256,7 +248,7 @@ class ViewController: UIViewController {
     //引き算
     @IBAction func subtractionBtn(_ sender: Any) {
         if opeFlg == 0{
-            culc(input: result)
+            calc(input: result)
             ope = "-"
             opeFlg = 1
             subBtn.layer.borderWidth = 2.0;
@@ -271,7 +263,7 @@ class ViewController: UIViewController {
     @IBAction func multiplicationBtn(_ sender: Any) {
         if opeFlg == 0{
             print(result)
-            culc(input: result)
+            calc(input: result)
             ope = "×"
             opeFlg = 1
             mulBtn.layer.borderWidth = 2.0;
@@ -285,7 +277,7 @@ class ViewController: UIViewController {
     //割り算
     @IBAction func divisionBtn(_ sender: Any) {
         if opeFlg == 0{
-            culc(input: result)
+            calc(input: result)
             ope = "÷"
             opeFlg = 1
             divBtn.layer.borderWidth = 2.0;
@@ -296,18 +288,19 @@ class ViewController: UIViewController {
         }
     }
 
+    //=ボタンの処理
     @IBAction func equalBtn(_ sender: Any) {
         
         if result == "0"{
         resultlbl.text = "エラー"
         }else{
-            culc(input: result)
+            calc(input: result)
             opeFlg = 1
+            bouderClear()
             }
         
         
         }
-    
 
     override func viewDidLoad() {
         super.viewDidLoad()
